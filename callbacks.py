c8641120e385cc63977a411b411f58fd8096f5ec
Isaac Yong <isaacyong0804@gmail.com>
2022-07-08 17:37:23 +0800

    Transformer implicit model, might break previous compatibility

--- callbacks.py
+++ callbacks.py
@@ -4,6 +4,7 @@ import numpy as np
 from composer.core.callback import Callback
 from matplotlib import animation
 from matplotlib import pyplot as plt
+from regex import P
 
 class SamplingCallback(Callback):
     def epoch_end(self, state, logger):
@@ -29,6 +30,19 @@ class SamplingCallback(Callback):
         animate.save(f"{folder}/sample-{state.timestamp.epoch}.gif")
         plt.close(fig)
 
+cifar_10_class = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
+
 class ImplicitSamplingCallback(Callback):
     def epoch_end(self, state, logger):
 
@@ -36,13 +50,17 @@ class ImplicitSamplingCallback(Callback):
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        gens = state.model.sample(num_samples=4).cpu().numpy()
-        gens = np.clip(gens / 2 + 0.5, 0, 1)
-        for i, gen in enumerate(gens, 0):
+        images, labels = state.model.sample(num_samples=4)
+        images = images.cpu().numpy()
+        labels = labels.cpu().numpy()
+        images = np.clip(images / 2 + 0.5, 0, 1)
+        for i, gen in enumerate(images, 0):
             fig = plt.figure()
             ims = []
+            im = plt.title(f"{cifar_10_class[labels[i]]}")
             for im in gen:
-                ims.append([plt.imshow(im, cmap="gray", animated=True)])
+                im = plt.imshow(im, cmap="gray", animated=True)
+                ims.append([im])
             animate = animation.ArtistAnimation(fig, ims, interval=100, blit=True, repeat_delay=1000)
             animate.save(f"{folder}/sample-{state.timestamp.epoch}-{i}.gif")
             plt.close(fig)
--- implicit_model.py
+++ implicit_model.py
@@ -14,9 +14,11 @@ from composer.optim.scheduler import CosineAnnealingWithWarmupScheduler
 from tqdm import tqdm
 from matplotlib import animation
 from matplotlib import pyplot as plt
+import math
 
 from data import get_cifar10
 from callbacks import ImplicitSamplingCallback
+from model import LinearAttention, PreNorm, SinusoidalPositionEmbeddings
 
 # Implicit Layer Implementation
 
@@ -36,13 +38,37 @@ class ResnetImplicitLayer(nn.Module):
         y = self.norm1(F.relu(self.conv1(z)))
         return self.norm3(F.relu(z + self.norm2(self.conv2(y) + x)))
 
+class TransformerLayer(nn.Module):
+    def __init__(self, dim, num_heads, expanding_ratio=4):
+        super().__init__()
+        self.cross_mha = nn.MultiheadAttention(dim, num_heads, batch_first=True)
+        self.mha = nn.MultiheadAttention(dim, num_heads, batch_first=True)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, dim*expanding_ratio),
+            nn.GELU(),
+            nn.Linear(dim*expanding_ratio, dim)
+        )
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+    
+    def forward(self, z, x):
+        z = z + x
+        attn, attn_weight = self.mha(z, z, z)
+        z = self.norm1(attn + z)
+        z = self.norm2(self.mlp(z) + z)
+        return z
+
 def anderson(f, x0, m=5, lam=1e-4, max_iter=50, tol=1e-2, beta=1.0, sampling=False):
     """ Anderson acceleration for fixed point iteration. """
-    bsz, d, H, W = x0.shape
-    X = torch.zeros(bsz, m, d*H*W, dtype=x0.dtype, device=x0.device)
-    F = torch.zeros(bsz, m, d*H*W, dtype=x0.dtype, device=x0.device)
-    X[:,0], F[:,0] = x0.view(bsz, -1), f(x0).view(bsz, -1)
-    X[:,1], F[:,1] = F[:,0], f(F[:,0].view_as(x0)).view(bsz, -1)
+    # bsz, d, H, W = x0.shape
+    # dim = d * H * W
+    bsz, d, n = x0.shape
+    dim = d*n
+    X = torch.zeros(bsz, m, dim, dtype=x0.dtype, device=x0.device)
+    F = torch.zeros(bsz, m, dim, dtype=x0.dtype, device=x0.device)
+    X[:,0], F[:,0] = x0.contiguous().view(bsz, -1), f(x0).contiguous().view(bsz, -1)
+    X[:,1], F[:,1] = F[:,0], f(F[:,0].view_as(x0)).contiguous().view(bsz, -1)
     
     H = torch.zeros(bsz, m+1, m+1, dtype=x0.dtype, device=x0.device)
     H[:,0,1:] = H[:,1:,0] = 1
@@ -59,7 +85,7 @@ def anderson(f, x0, m=5, lam=1e-4, max_iter=50, tol=1e-2, beta=1.0, sampling=Fal
         alpha = torch.solve(y[:,:n+1], H[:,:n+1,:n+1])[0][:, 1:n+1, 0]   # (bsz x n)
         
         X[:,k%m] = beta * (alpha[:,None] @ F[:,:n])[:,0] + (1-beta)*(alpha[:,None] @ X[:,:n])[:,0]
-        F[:,k%m] = f(X[:,k%m].view_as(x0)).view(bsz, -1)
+        F[:,k%m] = f(X[:,k%m].view_as(x0)).contiguous().view(bsz, -1)
         res.append((F[:,k%m] - X[:,k%m]).norm().item()/(1e-5 + F[:,k%m].norm().item()))
 
         if sampling:
@@ -114,6 +140,85 @@ class ResnetImplicitModel(nn.Module):
         x = self.conv2(x)
         return x
 
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x.transpose(0, 1)
+        x = x + self.pe[:x.size(0)]
+        x = x.transpose(0, 1)
+        return self.dropout(x)
+
+class TransformerImplicitModel(nn.Module):
+    def __init__(self, channels, latent_dim, num_heads, patch_size):
+        # Patchify
+        super().__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.patch_size = patch_size
+        self.latent_dim = latent_dim
+        self.patchify = nn.Conv2d(channels, latent_dim, kernel_size=self.patch_size, stride=self.patch_size)
+        self.pe = PositionalEncoding(latent_dim)
+
+        self.class_embedding = nn.Embedding(10, latent_dim, device=self.device)
+
+        f = TransformerLayer(latent_dim, num_heads)
+        self.deq = DEQFixedPoint(f, anderson, max_iter=50, tol=1e-2)
+
+        self.conv = nn.Conv2d(latent_dim//patch_size**2, channels, kernel_size=1)
+        self.classify = nn.Linear(latent_dim, 10)
+        self.fold = nn.Fold((32, 32), (2, 2), stride=(2, 2))
+
+    def forward(self, inputs, labels):
+
+        b, c, h, w = inputs.shape
+
+        labels = self.class_embedding(labels)
+        inputs = self.patchify(inputs).flatten(-2)
+        inputs = torch.cat([inputs, labels[:, :, None]], dim=-1).transpose(-1, -2).contiguous()
+        inputs = self.pe(inputs)
+
+        inputs = self.deq(inputs).transpose(-1, -2)
+
+        pred_labels = self.classify(inputs[:, :, -1])
+        inputs = inputs[:, :, :-1]
+        inputs = self.fold(inputs)
+        inputs = self.conv(inputs)
+
+        return inputs, F.softmax(pred_labels, dim=-1)
+    
+    def sampling(self, noise, labels_idx=None):
+
+        b, c, h, w = noise.shape
+
+        if not labels_idx:
+            labels_idx = torch.randint(0, self.class_embedding.num_embeddings-1, (b,), device=self.device).long()
+        labels = self.class_embedding(labels_idx)
+        noise = self.patchify(noise).flatten(-2)
+        noise = torch.cat([noise, labels[:, :, None]], dim=-1).transpose(-1, -2).contiguous()
+
+        noise = self.deq(noise, sampling=True)
+        noise = torch.cat(noise, dim=0).transpose(-1, -2)
+
+        noise = noise[:, :, :-1]
+        noise = self.fold(noise)
+        noise = self.conv(noise)
+        noise = noise.view(-1, b, c, h, w).permute(1, 0, 3, 4, 2)
+
+        return noise, labels_idx
+
 class DDIM(ComposerModel):
     def __init__(self, model, batch_size, image_size, channels, loss_type="huber"):
         super().__init__()
@@ -134,27 +239,32 @@ class DDIM(ComposerModel):
             raise NotImplementedError()
 
     def forward(self, batch):
-        images, _ = batch # Excluding labels
+        images, labels = batch 
         noise = torch.randn_like(images)
-        return self.model(noise)
+        return self.model(noise, labels)
 
-    def loss(self, generated_images, batch):
-        target, _ = batch
-        loss = self.criterion(generated_images, target)
-        return loss
+    def loss(self, output, batch):
+        images, labels = batch
+        gen_images, pred_labels = output
+        recon_loss = self.criterion(gen_images, images)
+        cls_loss = F.cross_entropy(pred_labels, labels)
+        return recon_loss + cls_loss
 
     @torch.no_grad()
     def sample(self, num_samples=4):
         noise = torch.randn(num_samples, self.channels,
                             self.image_size, self.image_size, device=self.device)
+        """
         noise = self.model.norm1(self.model.conv1(noise)).contiguous()
         noise = self.model.deq(noise, sampling=True)
         noise = torch.cat(noise, dim=0)
         noise = self.model.conv2(self.model.norm2(noise))
         # convert to [num_samples, batch_size, image_size, image_size, channels]
         noise = noise.view(-1, num_samples, self.channels, self.image_size, self.image_size).permute(1, 0, 3, 4, 2) 
-        print(noise.shape)
-        return noise
+        """
+        images, labels = self.model.sampling(noise)
+
+        return images, labels
 
 if __name__ == "__main__":
 
@@ -164,18 +274,24 @@ if __name__ == "__main__":
     latent_dim = 16
     inner_dim = 64
     num_groups = 8
+
+    num_heads = 8 
+    patch_size = 2
+
     initial_lr = 1e-3
     final_lr = 1e-5
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
     train_dataset, _ = get_cifar10(gray_scale=(channels==1))
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size, shuffle=True, num_workers=2, drop_last=True)
     
-    model = ResnetImplicitModel(channels=channels, latent_dim=latent_dim, inner_dim=inner_dim, num_groups=num_groups).to(device)
+    # model = ResnetImplicitModel(channels=channels, latent_dim=latent_dim, inner_dim=inner_dim, num_groups=num_groups).to(device)
+    model = TransformerImplicitModel(channels, latent_dim, num_heads, patch_size)
     model = DDIM(model, batch_size, image_size, channels, loss_type="huber")
     optimizer = optim.Adam(model.parameters(), lr=initial_lr)
-    run_name = "ddim-gray"
+    run_name = "ddim-transformer-gray"
 
     trainer = Trainer(
         model=model,
